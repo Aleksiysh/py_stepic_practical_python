55e2d67cf0f841731646602be5daa6ee4905de79
Aleksey <aleks-sh@mail.ru>
2020-03-02 14:14:57 +0300

    4.1.11 copy

---
+++ 4.1.stringMetods/4.1.11 copy.py
@@ -0,0 +1,26 @@
+
+def foo(a, line):
+
+    ops = {'+': lambda a, b: a + b,
+           '-': lambda a, b: a - b,
+           '*': lambda a, b: a * b,
+           '/': lambda a, b: a / b
+           }
+    return ops[line[0]](a, int(line[1]))
+
+
+def main():
+    l = []
+    while True:
+        str = input()
+        if str == '.':
+            break
+        l.append(str.split())
+    a = float(input())
+    for i in l:
+        a = foo(a, i)
+    print(a)
+
+
+if __name__ == '__main__':
+    main()
