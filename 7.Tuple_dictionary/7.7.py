ec58282a1543401847f0870dccada4e3b365fbb2
Aleksey <aleks-sh@mail.ru>
2020-03-02 06:46:21 +0300

    2

--- 7.Tuple_dictionary/7.7.py
+++ 7.Tuple_dictionary/7.7.py
@@ -1,15 +1,15 @@
 def code_number(num):
     line = ''
-    description = {1: "one",
-               2: "two",
-               3: "three",
-               4: "four",
-               5: "five",
-               6: "six",
-               7: "seven",
-               8: "eight",
-               9: "nine",
-               0: "zero"}
+    description = { 1: "one",
+                    2: "two",
+                    3: "three",
+                    4: "four",
+                    5: "five",
+                    6: "six",
+                    7: "seven",
+                    8: "eight",
+                    9: "nine",
+                    0: "zero"}
     for n in str(num):
         line+=( description[int(n)]+' ')
     return line[:-1]
